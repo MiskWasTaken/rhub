55024173c67d61a209cebae33cbe006825cda44e
WolfTheGamer <58915582+WolfTheGamer@users.noreply.github.com>
2022-04-29 00:07:12 +0200

    main: Update

--- main.lua
+++ main.lua
@@ -2,41 +2,27 @@
 -- Made by Misk#4044
 -- Love <3
 
-function shortnumberstring(a)local b={{1,""},{1e3,"k"},{1e6,"m"},{1e9,"g"},{1e12,"t"}}for c,d in ipairs(b)do if d[1]<=a+1 then b.use=c end end;local e=string.format("%.1f",a/b[b.use][1])if tonumber(e)>=1e3 and b.use<#b then b.use=b.use+1;e=string.format("%.1f",tonumber(e)/1e3)end;return e..b[b.use][2]end
-function ifNotFile(a,b)if isfile(a)==false then writefile(a,b)end end
-function tableToString(a)local b='{'local c=false;for d,e in pairs(a)do if c==false then c=true else b=b..','end;b=b..e end;return b..'}'end
+-- Starting variables
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "RHub", HidePremium = false, SaveConfig = true, ConfigFolder = "RHubFolderv1"})
 
-if isfolder('rhubfolder') == false then makefolder('rhubfolder') end
-ifNotFile('rhubfolder/userinfo.png', "{'"..tostring(game:GetService("RbxAnalyticsService"):GetClientId()).."'}")
-ifNotFile('rhubfolder/settings.png','{Enum.KeyCode.KeypadMinus}')
-ifNotFile('rhubfolder/scripts.txt', '{""}')
-ifNotFile('rhubfolder/README.md', [[{
-  {
-    ['NAME'] = 'script-guide'; -- Shown name
-    ['SCRIPT'] = function() print('Hello World!') end
-  }
-}]])
+-- Main
+local Main = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Main:AddButton({
+	Name = "Show Console",
+	Callback = function()
+    rconsoleclear()
+    rconsoleprint('@@RED@@')
+    rconsoleprint('RHub Console')
+  end    
+})
 
-local settingstable = loadstring('return '..readfile("rhubfolder/settings.png"))()
-
-local a=loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenDeno/Venyx-UI-Library/main/source.lua"))()local b=a.new("RHub",5013109572)
-getgenv().RHUBGUI = b
-
-local Settings = b:addPage("Settings", 5012544386):addSection("Settings")
-Settings:addKeybind("Toggle Keybind", settingstable[1], function() b:toggle() end)
-Settings:addButton('"RHUBGUI" is a getgenv value', nil)
-Settings:addButton('Close gui', function() b:toggle() wait(1) game.CoreGui['RHub']:Destroy() end)
-
-local scripts = loadstring('return '..readfile("rhubfolder/scripts.txt"))()
-local def = {""}
-
-if scripts[1] == def[1] then else
-    local scriptspage = b:addPage('Scripts', 5012539805)
-    for _,v in pairs(scripts) do
-        local x = scriptspage:addSection(v["NAME"])
-        x:addButton('Execute', function() v['SCRIPT']() end)
-    end
-end
+-- Init
+OrionLib:Init()
 
 --⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 --⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣶⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
