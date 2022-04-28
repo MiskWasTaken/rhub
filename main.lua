b13514649e2d7e0e354a6b3b83ddeb2c61f6c3fa
WolfTheGamer <58915582+WolfTheGamer@users.noreply.github.com>
2022-04-28 21:41:27 +0200

    main:

--- main.lua
+++ main.lua
@@ -2,9 +2,7 @@
 -- Made by Misk#4044
 -- Love <3
 
--- Testing AC
--- 1
-
+function shortnumberstring(a)local b={{1,""},{1e3,"k"},{1e6,"m"},{1e9,"g"},{1e12,"t"}}for c,d in ipairs(b)do if d[1]<=a+1 then b.use=c end end;local e=string.format("%.1f",a/b[b.use][1])if tonumber(e)>=1e3 and b.use<#b then b.use=b.use+1;e=string.format("%.1f",tonumber(e)/1e3)end;return e..b[b.use][2]end
 function ifNotFile(a,b)if isfile(a)==false then writefile(a,b)end end
 function tableToString(a)local b='{'local c=false;for d,e in pairs(a)do if c==false then c=true else b=b..','end;b=b..e end;return b..'}'end
 
