f0fdfb4a0f3e3ed2f4bd58a4a5792cae25ed0885
WolfTheGamer <58915582+WolfTheGamer@users.noreply.github.com>
2022-04-28 21:04:05 +0200

    Update

--- main.lua
+++ main.lua
@@ -2,10 +2,10 @@
 -- Made by Misk#4044
 -- Love <3
 
-print('caca')
-
 function ifNotFile(a,b)if isfile(a)==false then writefile(a,b)end end
 function tableToString(a)local b='{'local c=false;for d,e in pairs(a)do if c==false then c=true else b=b..','end;b=b..e end;return b..'}'end
+
+-- Test
 
 if isfolder('rhubfolder') == false then makefolder('rhubfolder') end
 ifNotFile('rhubfolder/userinfo.png', "{'"..tostring(game:GetService("RbxAnalyticsService"):GetClientId()).."'}")
